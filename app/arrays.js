05e88bf2350f79dbf4da36e66540e91799b53d95
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-02-23 15:35:39 -0800

    [solved] remove in arrays.js

--- app/arrays.js
+++ app/arrays.js
@@ -2,68 +2,74 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.arraysAnswers = {
 
-  indexOf : function(arr, item) {
-    for (var i = 0; i < arr.length; i++) {
-      if(arr[i] === item){
-        return i;
+    indexOf: function(arr, item) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === item) {
+                return i;
+            }
+        }
+        return -1;
+    },
+
+    sum: function(arr) {
+        var sum = 0;
+        for (var i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    },
+
+    remove: function(arr, item) {
+      var result = [];
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] !== item) {
+          result.push(arr[i]);
+        }
       }
+      return result;
+    },
+
+    removeWithoutCopy: function(arr, item) {
+
+    },
+
+    append: function(arr, item) {
+
+    },
+
+    truncate: function(arr) {
+
+    },
+
+    prepend: function(arr, item) {
+
+    },
+
+    curtail: function(arr) {
+
+    },
+
+    concat: function(arr1, arr2) {
+
+    },
+
+    insert: function(arr, item, index) {
+
+    },
+
+    count: function(arr, item) {
+
+    },
+
+    duplicates: function(arr) {
+
+    },
+
+    square: function(arr) {
+
+    },
+
+    findAllOccurrences: function(arr, target) {
+
     }
-    return -1;
-  },
-
-  sum : function(arr) {
-    var sum = 0;
-    for (var i = 0; i < arr.length; i++) {
-      sum += arr[i];
-    }
-    return sum;
-  },
-
-  remove : function(arr, item) {
-
-  },
-
-  removeWithoutCopy : function(arr, item) {
-
-  },
-
-  append : function(arr, item) {
-
-  },
-
-  truncate : function(arr) {
-
-  },
-
-  prepend : function(arr, item) {
-
-  },
-
-  curtail : function(arr) {
-
-  },
-
-  concat : function(arr1, arr2) {
-
-  },
-
-  insert : function(arr, item, index) {
-
-  },
-
-  count : function(arr, item) {
-
-  },
-
-  duplicates : function(arr) {
-
-  },
-
-  square : function(arr) {
-
-  },
-
-  findAllOccurrences : function(arr, target) {
-
-  }
 };
