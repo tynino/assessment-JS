496054d16eed434a4c17f61a5c9e2f8fdae52a9c
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-02-23 15:24:10 -0800

    [solved] sum in arrays.js

--- app/arrays.js
+++ app/arrays.js
@@ -12,7 +12,11 @@ exports.arraysAnswers = {
   },
 
   sum : function(arr) {
-
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
   },
 
   remove : function(arr, item) {
